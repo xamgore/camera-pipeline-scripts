a0c60540df6422568de30760300eaa447bdb12c5
xamgore <xamgore@ya.ru>
2018-09-12 01:52:22 +0300

    Update gitignore

--- extract_info/get_dates.py
+++ extract_info/get_dates.py
@@ -6,5 +6,6 @@ sdcard = '/home/xi/sdcard/PRIVATE/AVCHD/BDMV/STREAM/'
 movies = os.listdir(sdcard)
 
 for f in movies:
-    info = media.parse(sdcard + f).to_data()['tracks'][0]
-    print(f'{f}:\t{info["recorded_date"]}\t{info["other_duration"][0]}')
+    tracks = media.parse(sdcard + f).to_data()['tracks']
+    info = [t for t in tracks if t['track_type'] == 'General'][0]
+    print(f'{f}:\t{info["recorded_date"]}\t{info.get("other_duration", ["..."])[0]}')
